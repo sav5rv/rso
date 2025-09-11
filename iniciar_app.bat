6b1166b28977096266b3e57968ff1a459433daa5
sav5rv <wsavioli@gmail.com>
2025-09-10 21:16:57 -0300

    bat acesso local

---
+++ iniciar_app.bat
@@ -0,0 +1,13 @@
+@echo off
+title INICIAR NUMERADOR RSO
+
+echo Iniciando o Back-end...
+start cmd /k "cd C:\Users\Wellington\Documents\proj\rso\rso\back && npm start"
+
+echo Abrindo o Front-end no navegador...
+timeout /t 5 >nul
+start http://127.0.0.1:5500/front/index.html
+
+REM start C:\Users\Wellington\Documents\proj\rso\rso\front\index.html
+
+exit
