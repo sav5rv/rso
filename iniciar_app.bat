d945f2b4abfcba2c292c8a8188767596f7e1a623
sav5rv <wsavioli@gmail.com>
2025-09-17 14:21:56 -0300

    msg

--- iniciar_app.bat
+++ iniciar_app.bat
@@ -6,7 +6,7 @@ start cmd /k "cd C:\Users\Wellington\Documents\proj\rso\rso\back && npm start"
 
 echo Abrindo o Front-end no navegador...
 timeout /t 5 >nul
-start http://127.0.0.1:3001/index.html
+start http://127.0.0.1:3001/
 
 REM start C:\Users\Wellington\Documents\proj\rso\rso\front\index.html
 
